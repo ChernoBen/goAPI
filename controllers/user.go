57fb2995186357df200defa22612c5f4a8a0af6f
fang <bneto@computacao.cua.ufmt.br>
2021-07-18 17:24:18 -0400

    feat:user auth

--- controllers/user.go
+++ controllers/user.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
@@ -22,12 +21,14 @@ func CreateUser(c *gin.Context) {
 	var input CreateUserInput
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	//converter string para byte of string
 	var pass = []byte(input.Password)
 	hash, err := bcrypt.GenerateFromPassword(pass, bcrypt.DefaultCost)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid pass"})
+		return
 	}
 	//criação
 	user := models.User{Email: input.Email, Password: string(hash)} //convertendo byte of string em string
@@ -40,31 +41,47 @@ type UserAuth struct {
 	Password string `json:"password" binding:"required"`
 }
 
+type Claims struct {
+	ID         uint `json:"ID"`
+	Authorized bool `json:"authorized"`
+	jwt.StandardClaims
+}
+
 func Login(c *gin.Context) {
+	privateKey := "oasijdoaidsoijs"
 	var userEntry UserAuth
 	if err := c.BindJSON(&userEntry); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	var user models.User
 	//trata se o id nao existe
+	fmt.Println(userEntry.Email)
 	if err := models.DB.Where("email = ?", userEntry.Email).First(&user).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
 	fmt.Println(user.Email)
-	var hashed = []byte(user.Password)
-	er := bcrypt.CompareHashAndPassword(hashed, []byte(userEntry.Password))
+	er := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userEntry.Password))
 	if er != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid password"})
+		return
 	}
-	atClaims := jwt.MapClaims{}
-	atClaims["authorized"] = true
-	atClaims["user_id"] = user.ID
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
-	at := jwt.NewWithClaims(jwt.SigningMethodES256, atClaims)
-	token, err := at.SignedString([]byte("chernoBen"))
-	if err != nil {
+	//atClaims := &Claims{
+	//	ID:         user.ID,
+	//	Authorized: true,
+	//	StandardClaims: jwt.StandardClaims{
+	//		ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
+	//	},
+	//}
+	at := jwt.New(jwt.SigningMethodHS256)
+	fmt.Println(at)
+	token, erro := at.SignedString([]byte(privateKey))
+	fmt.Println(erro)
+	if erro != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": token})
+
 }
