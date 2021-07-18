6976b09de1642a0335729e449d73999176aef8b4
fang <bneto@computacao.cua.ufmt.br>
2021-07-18 13:44:07 -0400

    feat:complete CRUD

--- controllers/book.go
+++ controllers/book.go
@@ -9,7 +9,7 @@ import (
 
 type CreateBookInput struct {
 	Title  string `json:"title" binding:"required"`
-	Author string `json:binding:"required"`
+	Author string `json:"author" binding:"required"`
 }
 
 func CreateBook(c *gin.Context) {
@@ -24,8 +24,50 @@ func CreateBook(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
 
+func FindBook(c *gin.Context) {
+	var book models.Book
+	//trata se o id nao existe
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 	models.DB.Find(&books)
 	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+	//obter model se existe
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record fail"})
+		return
+	}
+	//validação de entrada
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	models.DB.Model(&book).Updates(input)
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func Delete(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record fail"})
+		return
+	}
+	models.DB.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
--- main.go
+++ main.go
@@ -10,7 +10,12 @@ import (
 func main() {
 	r := gin.Default()
 	models.ConnectDataBase()
+
+	r.GET("/books/:id", controllers.FindBook)
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.Delete)
+
 	r.Run()
 }
