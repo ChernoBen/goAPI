6ee2e0f1ad1107a8d9824af0f70182057089e0f0
fang <bneto@computacao.cua.ufmt.br>
2021-07-18 17:25:10 -0400

    feat:user model

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID       string `json:"id"`
+	ID       uint   `json:"id" gorm:"primary_key"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
